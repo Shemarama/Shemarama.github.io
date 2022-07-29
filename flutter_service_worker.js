cf24fefcdf82ec350500348d24cdb06c18672a7b
Shem <s.hale@aggiemail.usu.edu>
2022-07-28 19:42:21 -0600

    update build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3ad11b89d7850267ebcff53d0a6ce229",
-"/": "3ad11b89d7850267ebcff53d0a6ce229",
-"main.dart.js": "661f09f24b428b473875e843db60ca04",
+"index.html": "3bd0dd45d821a61679d0eb52389c171d",
+"/": "3bd0dd45d821a61679d0eb52389c171d",
+"main.dart.js": "103c3b1bab9019a0108b5c825a246d0b",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
